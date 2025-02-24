adf2edcc6cf1aeb1895a190ae4cc7a30ae48a6d9
Gacha <jlgacha75@gmail.com>
2025-02-24 12:30:13 +0100

    Frontend posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,12 +8,47 @@
     @vite(['resources/css/app.css'])
 </head>
 <body>
-    <main class="min-h-screen flex justify-between bg-zinc-950 p-6">
+    <main class="flex justify-between h-screen bg-zinc-950 p-6">
         <section>
             <h1 class="text-xl text-center font-semibold text-white">Instagram</h1>
         </section>
-        <section>
-            <h1 class="text-white">Aqui van las imagenes</h1>
+        <section class="flex flex-col gap-4 overflow-y-scroll max-h-screen custom-scrollbar">
+            <article class="flex flex-col gap-2 border-b border-zinc-800 pb-4">
+                <div class="flex gap-2 items-center">
+                    <img class="w-12 h-12 rounded-full" src="{{asset('default-avatar.webp')}}" alt="User">
+                    <span class="text-white font-semibold">kingsleague</span>
+                    <span class="text-gray-400">• 13 h</span>
+                </div>
+                <img class="w-full max-h-[585px] rounded-sm" src="https://media.discordapp.net/attachments/919641744704954461/1343523983059980399/481566662_17957688542868892_3054614692097926808_n.jpg?ex=67bd95b7&is=67bc4437&hm=5a5568a6e688210ea11e86fce0d216ea5dd76068068e5b3bb1ea11a145449033&=&format=webp" alt="Post">
+                <div class="flex gap-4 items-center">
+                    <x-bi-heart class="text-white h-6 w-6 cursor-pointer hover:text-gray-400"/>
+                    <x-far-comment class="text-white h-6 w-6 cursor-pointer hover:text-gray-400"/>
+                </div>
+                <div>
+                    <span class="text-white">5574 Me gusta</span>
+                </div>
+                <div>
+                    <p class="text-white"><span class="text-white font-semibold">kingsleague</span> Cositas 💖</p>
+                </div>
+            </article>
+            <article class="flex flex-col gap-2 border-b border-zinc-800 pb-4">
+                <div class="flex gap-2 items-center">
+                    <img class="w-12 h-12 rounded-full" src="{{asset('default-avatar.webp')}}" alt="User">
+                    <span class="text-white font-semibold">kingsleague</span>
+                    <span class="text-gray-400">• 13 h</span>
+                </div>
+                <img class="w-full max-h-[585px] rounded-sm" src="https://media.discordapp.net/attachments/919641744704954461/1343523983059980399/481566662_17957688542868892_3054614692097926808_n.jpg?ex=67bd95b7&is=67bc4437&hm=5a5568a6e688210ea11e86fce0d216ea5dd76068068e5b3bb1ea11a145449033&=&format=webp" alt="Post">
+                <div class="flex gap-4 items-center">
+                    <x-bi-heart class="text-white h-6 w-6 cursor-pointer hover:text-gray-400"/>
+                    <x-far-comment class="text-white h-6 w-6 cursor-pointer hover:text-gray-400"/>
+                </div>
+                <div>
+                    <span class="text-white">5574 Me gusta</span>
+                </div>
+                <div>
+                    <p class="text-white"><span class="text-white font-semibold">kingsleague</span> Cositas 💖</p>
+                </div>
+            </article>
         </section>
         <section class="flex flex-col gap-6">
             <div class="flex gap-4 justify-between items-center">
