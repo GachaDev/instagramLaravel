cff63b8effdd4df25407b32b3fe88a0f7827a62d
Gacha <jlgacha75@gmail.com>
2025-02-25 11:30:09 +0100

    Update post.blade.php

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -9,23 +9,12 @@
 </head>
 <body>
     <main class="flex justify-center h-screen bg-zinc-950 p-6">
+        <section>
+            <h1><a class="text-xl text-center font-semibold text-white" href="{{route('home')}}">Instagram</a></h1>
+        </section>
         <div class="w-1/2 h-full flex items-center border-r border-zinc-800">
             <div class="border-r border-zinc-800 flex w-full">
                 <img class="w-full max-h-[585px] rounded-sm object-cover object-top" src={{asset($post->url_image)}} alt="Post">
-                {{-- <div class="flex gap-4 items-center">
-                    <form class="flex items-center" action="{{route('posts.like', $post->id)}}" method="POST">
-                        @csrf
-                        <button type="submit"><x-bi-heart class="text-white h-6 w-6 cursor-pointer hover:text-red-500"></x-bi-heart></button>
-                    </form>
-                    <a href="{{route('posts.get', $post->id)}}"><x-far-comment class="text-white h-6 w-6 cursor-pointer hover:text-gray-400"/></a>
-                </div>
-                <div>
-                    <span class="text-white">{{ $post->n_likes }} Me gusta</span>
-                </div>
-                <div class="flex flex-col gap-2">
-                    <p class="text-white max-w-prose"><span class="text-white font-semibold">{{ $post->user->name }}</span> {{ $post->description }}</p>
-                    <a href="{{route('posts.get', $post->id)}}" class="text-gray-400 cursor-pointer">Ver los {{ $post->comments->count() }} comentarios</a>
-                </div> --}}
             </div>
         </div>
         <div class="flex justify-between h-full flex-col">
